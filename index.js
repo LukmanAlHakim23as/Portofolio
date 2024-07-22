08acbbd1ada0a5093b7c8b7d8bd9e8670d848458
LukmanAlHakim23as <lukmanhakim.23as2gmail.com>
2024-07-22 16:39:00 +0700

    Menambahkan Section footer

--- index.js
+++ index.js
@@ -55,3 +55,23 @@ document.addEventListener('DOMContentLoaded', function() {
       window.location.href = cvUrl;
     });
   });
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('nav ul li a');
+  
+    for (const link of links) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
+  
